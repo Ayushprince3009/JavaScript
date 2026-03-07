f74d650374fed800a53c6970b60b3c9d42aa1350
Ayush Kumar <ayushkkumar840@gmail.com>
2026-03-07 20:41:42 +0000

    variables let var const

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 145678;    //cant be changged 
+let accountEmail = "ayushkkumar840@gmail.com";    //can be changed
+var accountPassword = "1234545678";    //can be changed  now var is not used
+accountCity = "Noida";
+accountCity = "delhi";
+
+//accountId = 2;        not allowed
+accountEmail = "ayush56@gmail.com";
+accountPassword = "234447";
+let accountSum;
+
+
+
+console.log(accountId);
+
+/*
+prefer not to use var cause of issue in block scope and functional scope
+
+*/
+console.table([accountEmail,accountId,accountPassword,accountCity,accountSum]);
+
+
