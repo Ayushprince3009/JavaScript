6a1c05e4cc5109e2c67f2968e5c9021106be2c62
Ayush Kumar <ayushkkumar840@gmail.com>
2026-03-07 21:01:35 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";  // treat all js code as newer version
+
+//alert(3+3);    //we are using node js not browser
+
+console.log("ayush");
+
+console.log(3+
+
+    5
+);   //not recommended  code readability should be followed
+
+let name = "Ayush";       //string usually we use double quote and single quote for characters
+let age = 24;              // integer data type
+let isGood = true;        //boolean value either true or false
+
+                        //null standalone value 
+                        //symbol when we talk about unique data 
+
+console.table([name,age,isGood]);
+
+console.log(typeof "ayush");
+console.log(typeof null);
+console.log(typeof undefined);
