efc011cc409c04f472bda09b9264ddad2363d181
Ayush Kumar <ayushkkumar840@gmail.com>
2026-03-07 21:20:18 +0000

    new

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+//let score = 33;
+// console.log(typeof 33);
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let score = "acv";
+// console.log(typeof(score));
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+// "333add" => NaN
+//"true" => 1
+
+let isGood = 1;
+
+let good = Boolean(isGood);
+console.log(good);
+console.log(typeof good);
+
+// 1=>true   0=>false
+//"" empty sring => false
+// string =>    true
+//nmnnnmnmnmnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
