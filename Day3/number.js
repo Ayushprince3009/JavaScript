b6c4b38474c2bfa5a8e19db1ffa4a56f14de39ab
Ayush Kumar <ayushkkumar840@gmail.com>
2026-03-08 22:21:31 +0000

    Uncomment code in number.js for better readability and add string.js with string manipulation examples

--- Day3/number.js
+++ Day3/number.js
@@ -1,20 +1,20 @@
-          DAY #3 STARTS NOW
+//           DAY #3 STARTS NOW
 
-let num = 34.43452535;
-// num.toFixed()   esme jo number jayegaa utna decimal ke baad digit ye lega aur koi float value rhega to usme se
-let c = num.toFixed(2);
-console.log(c);
-console.log(typeof (c));
+// let num = 34.43452535;
+// // num.toFixed()   esme jo number jayegaa utna decimal ke baad digit ye lega aur koi float value rhega to usme se
+// let c = num.toFixed(2);
+// console.log(c);
+// console.log(typeof (c));
 
-let str = "ILOVEMYCOUNTRY";    //agar o ka krte to first occurance deta hai
-let c = str.indexOf("C");
-console.log(c);
-console.log(str.indexOf("Z"));   //jo string me nhi hoga uska return -1 krega 
+// let str = "ILOVEMYCOUNTRY";    //agar o ka krte to first occurance deta hai
+// let c = str.indexOf("C");
+// console.log(c);
+// console.log(str.indexOf("Z"));   //jo string me nhi hoga uska return -1 krega 
 
-let name = "    ayush     ";
+// let name = "    ayush     ";
 
-// esee kahte hai method chaining esme hum ek sth 2 method ko use krte h 
-console.log(name.trim().toUpperCase());
+// // esee kahte hai method chaining esme hum ek sth 2 method ko use krte h 
+// console.log(name.trim().toUpperCase());
 
 
 
