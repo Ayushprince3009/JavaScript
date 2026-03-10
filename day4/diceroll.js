d63f8945320a4de05bdc204427399e51510cbcbb
Ayush Kumar <ayushkkumar840@gmail.com>
2026-03-10 17:57:21 +0000

    Add diceroll.js and poem.js files with functions for random number generation and poem output

---
+++ day4/diceroll.js
@@ -0,0 +1,5 @@
+function diceRoll(){
+    let num = Math.floor(Math.random()*6)+1;
+    console.log(num);
+}
+diceRoll();
---
+++ day4/poem.js
@@ -0,0 +1,7 @@
+function poem(){
+    console.log("Twinkle Twinkle little start");
+    console.log("How i wonder what you are");
+    console.log("up above in the sky");
+    console.log("ek mota hathi sair pe chala");
+}
+poem();
