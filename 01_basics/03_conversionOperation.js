6422409849af79cc3573641306078c7845eea004
Ayush Kumar <ayushkkumar840@gmail.com>
2026-03-10 06:07:54 +0530

    Enable type conversion examples in conversionOperation.js
    Uncommented code to demonstrate type conversion in JavaScript.

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,19 +1,19 @@
-//let score = 33;
-// console.log(typeof 33);
-// console.log(typeof score);
-// console.log(typeof(score));
+let score = 33;
+console.log(typeof 33);
+console.log(typeof score);
+console.log(typeof(score));
 
-// let score = "acv";
-// console.log(typeof(score));
-// console.log(typeof score);
+let score = "acv";
+console.log(typeof(score));
+console.log(typeof score);
 
-// let valueInNumber = Number(score);
-// console.log(valueInNumber);
-// console.log(typeof valueInNumber);
+let valueInNumber = Number(score);
 
-// "33" => 33
-// "333add" => NaN
-//"true" => 1
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+"33" => 33
+"333add" => NaN
+"true" => 1
 
 let isGood = 1;
 
@@ -21,7 +21,7 @@ let good = Boolean(isGood);
 console.log(good);
 console.log(typeof good);
 
-// 1=>true   0=>false
-//"" empty sring => false
-// string =>    true
-//nmnnnmnmnmnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
+1=>true   0=>false
+"" empty sring => false
+string =>    true
+nmnnnmnmnmnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
