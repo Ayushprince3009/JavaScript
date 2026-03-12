2116290e1b56249240266f31a61b535f329a5b73
Ayush Kumar <ayushkkumar840@gmail.com>
2026-03-12 23:20:33 +0000

    new

---
+++ Day5/forEach.js
@@ -0,0 +1,11 @@
+let arr =[1,2,3,4,5];
+
+// arr.forEach(function(el){
+//     console.log(el);
+// });
+
+function print(el){
+    console.log(el);
+}
+
+arr.forEach(print);
---
+++ Day5/map.js
@@ -0,0 +1,6 @@
+let arr =[1,2,3,4,5,6];
+
+let double = arr.map(function(el){
+    return el*2;
+});
+console.log(double);
