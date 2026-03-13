239a01f5e879fbc6ec0c16128bfd5bd648dac8ee
Ayush Kumar <ayushkkumar840@gmail.com>
2026-03-13 18:49:49 +0000

    new

---
+++ hiteshjsp-1/first.js
@@ -0,0 +1,3 @@
+console.log("Ayush");
+console.log("starting javascript");
+console.log("How to learn JS and from where to learn");
---
+++ hiteshjsp-1/variable.js
@@ -0,0 +1,28 @@
+const accountId = 123456789;      // cant be updated or changed
+
+let accountEmail = "ayushk@gmail.com";    //can be changed or updated 
+
+var accountPassword = "1234567";   //can be changed
+accountCity = "Noida";            //can be changed
+                // const keyword can not be changed. 
+//accountId = 2345;   //not allowed nut variable can be decleared like this 
+
+console.log(accountId);
+
+accountEmail="asdjfdfdjk@dfgnj.com";
+accountPassword = "876543";
+accountCity = "Delhi";
+
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+let accountState;
+console.log(accountState);
+
+
+/*
+prefer not to uise var duer to functional and block scope
+*/
