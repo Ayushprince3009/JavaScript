667fe437b6e3353f6ea28845d5aececf8628fd82
Ayush Kumar <ayushkkumar840@gmail.com>
2026-03-13 23:26:17 +0000

    newww

---
+++ hiteshjsp-1/dataTypes.js
@@ -0,0 +1,13 @@
+"use strict";   //treat all js code as newer version
+
+let a = 5;
+console.log(typeof a);
+
+let b = "aman";
+console.log(typeof(b));
+
+let c = true;
+console.log(typeof (c)); 
+
+const score = 400;
+console.log(score);
---
+++ hiteshjsp-1/numath.js
@@ -0,0 +1,25 @@
+// const score = 400;
+// console.log(score);
+
+// const bal = new Number(100);
+// console.log(bal);
+
+// let max = 20;
+// let min = 15;
+
+// console.log(Math.floor(Math.random() * (max - min +1)) + min );
+/*universal formula for finding random numbers between two 
+*/
+//    date =-=====================
+
+let myDate = new Date();
+console.log(myDate);
+console.log(myDate.toDateString());
+console.log(myDate.getDate());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toString());
+console.log(myDate.toTimeString());
