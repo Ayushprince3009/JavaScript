ab808dc289097b925ccd30267d4af25c27dedab5
Ayush Kumar <ayushkkumar840@gmail.com>
2026-03-16 20:35:18 +0000

    arrowfunction

--- hiteshjsp-1/arrow.js
+++ hiteshjsp-1/arrow.js
@@ -14,5 +14,17 @@ power(2,3);
 const cub = n => {             //agar ek hi argument ho to bina bracket ke hi kaam ho jayega 
     console.log(n*n*n);
 };
-
 cub(2);
+
+
+const mul = (a,b) => (
+    a*b
+);
+
+console.log(mul(4,5));
+
+//////implicit function    single line me return dega  arrow function is used o write small function 
+const cube = (n) => (n*n*n);
+console.log(cube(3));
+
+
