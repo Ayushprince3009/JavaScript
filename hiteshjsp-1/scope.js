35d58480f24f35c785492693193001a2d5c44753
Ayush Kumar <ayushkkumar840@gmail.com>
2026-03-16 14:47:26 +0000

    new

---
+++ hiteshjsp-1/scope.js
@@ -0,0 +1,18 @@
+if(true){
+    let a =10;
+    const b = 20;
+    var c= 30;
+}
+
+console.log(a);    // a and b print nhi goga due to scope
+console.log(b);
+console.log(c);     //esiliye hum var ko nhi use krte hai 
+
+//=====================intersting-----------------------------------
+
+
+
+console.log(addone(5));
+function addone(num){
+    return num+1
+}
