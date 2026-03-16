4ccd6e17e072707725b56d6d858a936a3972361e
Ayush Kumar <ayushkkumar840@gmail.com>
2026-03-16 14:19:51 +0000

    NEW

--- hiteshjsp-1/obj.js
+++ hiteshjsp-1/obj.js
@@ -13,10 +13,11 @@ const jsUser = {
 
 // console.log(jsUser[mySym]);  //wat to print symbol from object
 
-const obj1={1:"A",2:"B",3:"C",4:"D"};
-const obj2={5:"E",6:"F",7:"G",8:"H"};
+// const obj1={1:"A",2:"B",3:"C",4:"D"};
+// const obj2={5:"E",6:"F",7:"G",8:"H"};
 
 //const obj3={obj1 , obj2};
 //const obj3 = Object.assign({}, obj1, obj2);
-const obj3 = {...obj1, ...obj2};   //sprad operator se sb ko ek sth lane 
-console.log(obj3);
+// const obj3 = {...obj1, ...obj2};   //sprad operator se sb ko ek sth lane 
+// console.log(obj3);
+
