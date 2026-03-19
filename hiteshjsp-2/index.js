fc9bdc413f417d9b7d9acc1dad6f2c79ef0d300a
Ayush Kumar <ayushkkumar840@gmail.com>
2026-03-19 16:04:09 +0530

    new

--- hiteshjsp-2/index.js
+++ hiteshjsp-2/index.js
@@ -1,4 +1,18 @@
 const greeting = "hello world";
 for(greet of greeting){
-    console.log(`character is ${greet}`);
+    if(greet == " "){
+        break;
+    }
+    //console.log(`character is ${greet}`);
 }
+
+const num = [1,2,3,4,5,6,7,8,9];
+for(number of num){
+    //console.log(number);
+}
+
+//filter
+
+const newNum = num.filter((n) => n>4 )
+console.log(newNum);
+
