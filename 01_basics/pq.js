b97a3b53edc96bc26abc04ac20ba64cca85eb903
Ayush Kumar <ayushkkumar840@gmail.com>
2026-03-29 00:13:43 +0530

    new

---
+++ 01_basics/pq.js
@@ -0,0 +1,19 @@
+let name = "Ayush Kumar";
+console.log(name.length);
+// console.log(name[0]);
+console.log(name[name.length - 1]);
+console.log(name[10]);
+
+let a = "apnacollege"+123;
+console.log(a);
+
+let b = "";
+let c = " ";
+
+console.log(b.length);
+console.log(c.length);
+
+let first = "ayush";
+let last = "kumar";
+console.log(first+last);
+console.log(first+" "+last);
