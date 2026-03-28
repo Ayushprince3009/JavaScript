dc807bfa569f165c9e60b95f02e00872871a7115
Ayush Kumar <ayushkkumar840@gmail.com>
2026-03-29 00:31:03 +0530

    new

---
+++ 01_basics/popcorn.js
@@ -0,0 +1,17 @@
+console.log("Enter size:");
+let size = prompt("Enter size---");
+if(size === "XL"){
+    console.log("Price is Rs.250");
+}
+else if(size === "L"){
+    console.log("Price is Rs.200");
+}
+else if(size === "M"){
+    console.log("Price is Rs.100");
+}
+else if(size === "S"){
+    console.log("Price is Rs.50");
+}
+else{
+    console.log("Enter valid size");
+}
