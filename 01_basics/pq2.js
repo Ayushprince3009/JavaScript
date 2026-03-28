e85f6ada3c5fc4161651e6ecd3cfc8cb909cb5a8
Ayush Kumar <ayushkkumar840@gmail.com>
2026-03-29 00:49:09 +0530

    pq2

---
+++ 01_basics/pq2.js
@@ -0,0 +1,5 @@
+console.log("Enter username:");
+let username = prompt("Enter username");
+console.log("Enter age:");
+let age = Number(prompt("Enter age:"));
+console.log(`${username} is ${age} years old.`);
