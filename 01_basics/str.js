a46a6aadaf087f6d5dca9671667159737f2836fb
Ayush Kumar <ayushkkumar840@gmail.com>
2026-03-29 00:35:02 +0530

    new

---
+++ 01_basics/str.js
@@ -0,0 +1,8 @@
+console.log("Enter string");
+let str = prompt("Enter string");
+if(str[0] === "a" && str.length > 3){
+    console.log("Good String");
+}
+else{
+    console.log("Not a good string");
+}
