4b127433623f8a6771bd2558a6f9ae609f70433c
Ayush Kumar <ayushkkumar840@gmail.com>
2026-03-29 00:55:05 +0530

    pq3

---
+++ 01_basics/pq3.js
@@ -0,0 +1,19 @@
+console.log("Enter number:");
+let quarter = Number(prompt("Enter number"));
+
+switch(quarter){
+    case 1:
+        console.log("January,February,March");
+        break;
+    case 2:
+        console.log("April,May,June");
+        break;
+    case 3:
+        console.log("July,August,September");
+        break;
+    case 4:
+        console.log("October,November,December");
+        break;
+    default:
+        console.log("Enter valid quarter");               
+}
