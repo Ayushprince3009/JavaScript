fe1b11f4860495d94f402d5f7c7813457793d36e
Ayush Kumar <ayushkkumar840@gmail.com>
2026-03-29 00:24:52 +0530

    new

---
+++ 01_basics/traffic.js
@@ -0,0 +1,11 @@
+console.log("Enter color");
+let color = prompt("Enter color");
+if(color  === "Red"){
+    console.log("STOP");
+}
+if(color === "Yellow"){
+    console.log("SLOW DOWN");
+}
+if(color === "Green"){
+    console.log("GO GOA GONE");
+}
