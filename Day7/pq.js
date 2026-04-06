ec00ca338ead7175028e468231131da0f9c002a7
Ayush Kumar <ayushkkumar840@gmail.com>
2026-04-06 17:06:08 +0530

    practice question

---
+++ Day7/pq.js
@@ -0,0 +1,97 @@
+// let fruit = ["apple","Banana","mango"];
+// console.log(fruit[1]);
+
+// let fruits = ["Apple","Banana"];
+
+// console.log(fruits);
+
+// fruits.push("Mango");
+// fruits.unshift("Pineapple");
+
+// console.log(fruits);
+
+// let fruitss = ["Apple","Banana"];
+// fruitss.splice(1,1,"Kiwi");
+// console.log(fruitss);
+
+// let colors = ["Green","Yellow"];
+// colors.splice(1,0,"Red","Blue");
+// console.log(colors);
+
+// let items = [1,2,3,4,5,6];
+// console.log(items.slice(1,4));
+
+// let name = ["Zara","Arjun","Mirza","Bhavya"];
+// name.sort();
+// console.log(name);
+
+// name.reverse();
+// console.log(name);
+
+// name.sort().reverse();
+// console.log(name);
+
+// let arr = [1,2,3,4];
+
+// let newA = arr.map(function(el){
+//     return el*el;
+// });
+
+// console.log(newA);
+
+// let arr = [5,12,8,20,3];
+
+// let newarr = arr.filter(function(el){
+//     return el > 10;
+// });
+// console.log(newarr);
+
+// let newArr = arr.filter((val) => {
+//     return val > 10;
+// })
+// console.log(newArr);
+
+// let arr = [10,20,30];
+
+// let sum = arr.reduce(function(acc,val){
+//     return acc+val;
+// },0);
+
+// console.log(sum);
+
+// let arr = [12,15,1,3,8,20];
+// let res = arr.find(function(val){
+//     return val <10;
+// });
+
+// console.log(res);
+
+
+// let arr = [45,60,28,90];
+
+// let res = arr.some(function(val){
+//     return val<35;
+// });
+// console.log(res);
+
+// let arr = [2,4,6,8,10,11];
+
+// let res = arr.every((val) =>{
+//     return val%2 === 0;
+// });
+// console.log(res);
+
+//DESTRUCTURE ARRAY AND GET OUT FIRST NAME AND LAST NAME
+// let fullName = ["Harsh" , "Sharma"];
+
+// let[firstName,lastName] = fullName;
+// console.log(firstName);
+// console.log(lastName);
+
+//spread operator for merging two arrays 
+// let a = [1,2];
+// let b = [3,4];
+
+// let newA = [...a , ...b];
+// console.log(newA);
+
