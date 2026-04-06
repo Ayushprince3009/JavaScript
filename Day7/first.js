a255ad6de7ec59a134f20d5719239462150090ef
Ayush Kumar <ayushkkumar840@gmail.com>
2026-04-06 10:12:22 +0530

    new

---
+++ Day7/first.js
@@ -0,0 +1,12 @@
+let arr= [1,2,3,4,5,6,7];
+
+// arr.forEach(function(val){
+//     console.log(val*2);
+// });
+
+let newArr=arr.map(function(val){
+    //console.log(val+5);
+    return 12;
+});
+
+console.log(newArr);
