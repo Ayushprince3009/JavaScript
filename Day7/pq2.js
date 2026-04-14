c31d2217744b590b53771aa49b8302eaec930aea
Ayush Kumar <ayushkkumar840@gmail.com>
2026-04-14 21:33:15 +0530

    new

---
+++ Day7/pq2.js
@@ -0,0 +1,9 @@
+let country = ["Australia","Germany","United States Of America","India"];
+longestCountry(country);
+
+function longestCountry(country){
+    let long = country[0];
+    for(let i=1; i<country.length; i++){
+        if()
+    }
+}
